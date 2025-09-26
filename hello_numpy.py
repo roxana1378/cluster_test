5302b8e0f0120a25c96d0293e900ac4f478371c2
Roxana Darvishi <roxana_darvishi@sfu.ca>
2025-09-26 14:21:34 -0700

    Add files via upload

---
+++ hello_numpy.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+def main():
+    x = np.array([1, 2, 3, 4, 5])
+    print("Array:", x)
+    print("Mean:", np.mean(x))
+    print("Standard deviation:", np.std(x))
+
+if __name__ == "__main__":
+    main()
